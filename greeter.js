ac088cfc4ed78dba3299b90479c4478006278cd1
Sven Olding <sven.olding@gmx.de>
2018-12-04 11:27:49 +0100

    add type annotation, compile error

--- greeter.js
+++ greeter.js
@@ -1,7 +1,5 @@
-function greeter(person: string) {
+function greeter(person) {
     return "Hallo " + person;
 }
-
-let user = [0, 1, 2];
-
+var user = [0, 1, 2];
 document.body.innerHTML = greeter(user);
