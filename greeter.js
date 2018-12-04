4f7c09cafbf2de2d68b47586407cc1e06382fe67
Sven Olding <sven.olding@gmx.de>
2018-12-04 11:35:12 +0100

    add class Student

--- greeter.js
+++ greeter.js
@@ -1,5 +1,14 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = (firstName + " " + middleInitial + " " + lastName).trim();
+    }
+    return Student;
+}());
 function greeter(person) {
     return "Hallo " + person.firstName + " " + person.lastName;
 }
-var user = { firstName: "Max", lastName: "Mustermann" };
+var user = new Student("Max", "M.", "Mustermann");
 document.body.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,3 +1,10 @@
+class Student implements Person {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = (firstName + " " + middleInitial + " " + lastName).trim();
+    }
+}
+
 interface Person {
     firstName: string;
     lastName: string;
@@ -7,7 +14,7 @@ function greeter(person: Person) {
     return "Hallo " + person.firstName + " " + person.lastName;
 }
 
-let user =  { firstName: "Max", lastName: "Mustermann" };
+let user =  new Student("Max", "M.", "Mustermann");
 
 document.body.innerHTML = greeter(user);
 
